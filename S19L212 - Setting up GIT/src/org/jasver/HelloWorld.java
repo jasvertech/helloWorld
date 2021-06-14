427db18bc389f2075cc89372d17749faf89c5e97
Jason Vernon <Jason Vernon@JASVER1>
2021-06-14 15:54:27 -0400

    First commit

---
+++ S19L212 - Setting up GIT/src/org/jasver/HelloWorld.java
@@ -0,0 +1,18 @@
+package org.jasver;
+
+
+/**
+ * Setting up GIT
+ * 
+ * @author Jason Vernon
+ *
+ */
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+
+	}
+
+}
